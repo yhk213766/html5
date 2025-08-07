6c575f8316a0cb570eb5a593a44fe852fab8286c
yhk213766 <yhk213@lx.or.kr>
2025-08-07 17:27:37 +0900

    11

--- humortube/vite.config.js
+++ humortube/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/html/humortube/',
+  base:'/html5/humortube/',
   plugins: [react()],
 })
