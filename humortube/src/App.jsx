4e25fb9781dfb67e6061247a0b486015d61ec498
yhk213766 <yhk213@lx.or.kr>
2025-08-07 16:38:48 +0900

    유머튜브

---
+++ humortube/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  
+  return (
+    <>
+    <div className="center_div">
+      <h1>유튜브 영상 모음</h1>
+
+     <h2>아기와나</h2>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/RHZb_u3VOco?si=Fx4itGjCtYdM22Ea&amp;start=50" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+   
+     <h2>미용실</h2>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/cf5mRYdCMqg?si=-bNPFxAKVHlWIDrf&amp;start=20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    </>
+  )
+}
+
+export default App
